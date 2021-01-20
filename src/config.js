3c5de86b77af6b80a73f27d76e46e54f7e0984c0
Leonardo <20160527@aloe.ulima.edu.pe>
2021-01-20 01:39:38 -0500

    env vars

--- src/config.js
+++ src/config.js
@@ -1,10 +1,8 @@
 var config = {
   region: "la2",
-  champRotation: "platform/v3/champion-rotations",
   summoner: "summoner",
-  match: "match",
-  summoner_url: "https://su6h7tt224.execute-api.sa-east-1.amazonaws.com/rgapi",
-  summoner_rank: "https://dxgjzvq0vh.execute-api.sa-east-1.amazonaws.com/rgapi",
+  summoner_url: process.env.REACT_APP_SUMMONER_URL,
+  summoner_rank: process.env.REACT_APP_RANK_URL,
 };
 
 //No cambiar o no podra detectar las variables del Array
